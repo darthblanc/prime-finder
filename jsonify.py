5e6877c46906346e754b021bcfa11d82ea489d10
Clement-Andi Edet <cedet@udallas.edu>
2025-10-11 18:34:53 -0500

    new custom jsonify functions

--- jsonify.py
+++ jsonify.py
@@ -1,11 +1,27 @@
 import json
 
-def jsonify(id, start, end, known_primes):
+def jsonify(id, start, end):
     return json.dumps(
         {
             "id": str(id),
             "start": int(start),
             "end": int(end),
-            "known_primes": [int(x) for x in known_primes]
+        }
+    )
+
+def jsonify_essential_primes(essential_primes):
+    return json.dumps(
+        {
+            "known_primes": list(map(int, essential_primes))
+        }
+    )
+
+def jsonify_worker_results(id, start, end, primes):
+    return json.dumps(
+        {
+            "id": str(id),
+            "start": int(start),
+            "end": int(end),
+            "known_primes": list(map(int, primes))
         }
     )
