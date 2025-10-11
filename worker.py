d48f04e4fa4726504d4b8b9fe8e87236bee97612
Clement-Andi Edet <cedet@udallas.edu>
2025-10-11 18:35:40 -0500

    worker now listens for the essential primes

--- worker.py
+++ worker.py
@@ -3,12 +3,21 @@ import json
 from prime import get_primes
 import sys
 import datetime
-from jsonify import jsonify
+from jsonify import jsonify_worker_results
 
 r = redis.Redis()
 
 def worker(name):
     print(f"{datetime.datetime.now()}: Worker: ({name}) started running")
+    known_primes = {}
+
+    pubsub = r.pubsub()
+    pubsub.subscribe("init")
+
+    for msg in pubsub.listen():
+        if msg["type"] == "message":
+            known_primes = json.loads(msg["data"].decode())["known_primes"]
+            break
 
     while True:
         task_data = r.brpop("primes:tasks")[1]
@@ -16,13 +25,12 @@ def worker(name):
         id = json_data["id"]
         start = json_data["start"]
         end = json_data["end"]
-        known_primes = json_data["known_primes"]
 
         primes = get_primes(start, end, known_primes)
 
         print(f"{datetime.datetime.now()}: ({name}) just processed: [{start}, {end}] -> Found {len(primes)} primes")
 
-        r.lpush("primes:results", jsonify(id, start, end, primes))
+        r.lpush("primes:results", jsonify_worker_results(id, start, end, primes))
 
 if "__main__" == __name__:
     try:
