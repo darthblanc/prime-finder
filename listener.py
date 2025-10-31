1dab98c3232b339406af0157caff6b1a952576f3
Clement-Andi Edet <cedet@udallas.edu>
2025-10-30 23:11:52 -0500

    added primes metadata to help track which tasks are being done

--- listener.py
+++ listener.py
@@ -2,13 +2,12 @@ import redis
 import json
 import sys
 import datetime
-from collections import defaultdict
 
 r = redis.Redis()
 
 def listener(name):
     print(f"{datetime.datetime.now()}: Listener: ({name}) started running")
-    prime_counts = defaultdict(int)
+    primes_metadata = {} # {id: [count, start, end]}
 
     while True:
         task_data = r.brpop("primes:results")[1]
@@ -18,12 +17,23 @@ def listener(name):
         end = json_data["end"]
         primes = json_data["known_primes"]
 
-        prime_counts[id] += len(primes)
+        if id in primes_metadata:
+            primes_metadata[id][0] += len(primes)
+            if start < primes_metadata[id][1]:
+                primes_metadata[id][1] = start
+            if end > primes_metadata[id][2]:
+                primes_metadata[id][2] = end
+        else:
+            primes_metadata[id] = [len(primes), start, end]
 
-        print(f"{datetime.datetime.now()}: ({name}) just processed: [{start}, {end}] with id: {id}: Counted {prime_counts[id]} primes")
+        if start == end == 0:
+            print(f"Task with {id}: has {primes_metadata[id][0]} primes")
 
 if "__main__" == __name__:
     try:
-        listener(sys.argv[1])
+        if len(sys.argv) == 2:
+            listener(sys.argv[1])
+        else:
+            listener("Listener")
     except Exception as e:
         raise Exception(f"panic: {e}")
